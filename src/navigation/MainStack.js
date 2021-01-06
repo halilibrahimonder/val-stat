9a039a5858eb952b166fdb1a8d5682bdc9a3f092
Halil Ibrahim <halilibrahimonder@icloud.com>
2021-01-07 00:00:08 +0300

    added new screen into MainStack and new back button for app.

--- src/navigation/MainStack.js
+++ src/navigation/MainStack.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
 import HomeScreen from '../screens/Home';
+import CareerTab from './CareerTab';
+import ScoreTab from './ScoreTab';
 import {createStackNavigator} from '@react-navigation/stack';
-import {Image} from 'react-native';
+import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
 import {Avatar} from 'react-native-elements';
 
 export default class MainStack extends Component {
   render() {
+    const getHeaderTitle = (route) => {
+      const routeName = getFocusedRouteNameFromRoute(route) ?? 'Career';
+      switch (routeName) {
+        case 'Career':
+          return 'Career';
+        case 'Characters':
+          return 'Characters';
+      }
+    };
     const Stack = createStackNavigator();
     return (
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerTransparent: true,
+          headerLeft: () => (
+            <Avatar
+              source={require('../assets/images/icons/back.png')}
+              onPress={() => this.props.navigation.goBack()}
+              placeholderStyle={{backgroundColor: 'transparan'}}
+            />
+          ),
+        }}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -16,6 +38,18 @@ export default class MainStack extends Component {
             headerShown: false,
             title: '',
           }}
+        />
+
+        <Stack.Screen
+          name="CareerTab"
+          component={CareerTab}
+          options={{title: ''}}
+        />
+
+        <Stack.Screen
+          name="ScoreTab"
+          component={ScoreTab}
+          options={{title: ''}}
         />
       </Stack.Navigator>
     );
