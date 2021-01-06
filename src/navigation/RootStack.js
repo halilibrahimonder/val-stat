6c3ca4c2015f9b36e93d272a8a06383995cc78b3
Halil Ibrahim <halilibrahimonder@icloud.com>
2021-01-07 00:01:43 +0300

    restructured RootStack for new modals.

--- src/navigation/RootStack.js
+++ src/navigation/RootStack.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
+import PerformanceModal from '../screens/Performance/modal';
+import CharactersModal from '../screens/Characters/modal';
 import MainStackScreen from './MainStack';
 
 export default class AppContainer extends Component {
@@ -11,6 +14,14 @@ export default class AppContainer extends Component {
       <NavigationContainer>
         <RootStack.Navigator mode="modal" headerMode="none">
           <RootStack.Screen name="Main" component={MainStackScreen} />
+          <RootStack.Screen
+            name="PerformanceModal"
+            component={PerformanceModal}
+          />
+          <RootStack.Screen
+            name="CharactersModal"
+            component={CharactersModal}
+          />
         </RootStack.Navigator>
       </NavigationContainer>
     );
