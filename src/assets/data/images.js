40d3716ec1e87bb886c9ad37cafd0e1536421c48
Halil Ibrahim <halilibrahimonder@icloud.com>
2021-01-09 18:56:03 +0300

    added uris for images.

---
+++ src/assets/data/images.js
@@ -0,0 +1,140 @@
+const images = {
+  characters: {
+    Raze: {
+      uri: require('../images/agents/icons/raze2.png'),
+      color: '#EABD74',
+      full_small: require('../images/agents/fullscreen-small/raze.png'),
+    },
+    Breach: {
+      uri: require('../images/agents/icons/breach2.png'),
+      color: '#AE806A',
+      full_small: require('../images/agents/fullscreen-small/breach.png'),
+    },
+    Brimstone: {
+      uri: require('../images/agents/icons/brimstone2.png'),
+      color: '#578DB3',
+      full_small: require('../images/agents/fullscreen-small/brimstone.png'),
+    },
+    Cypher: {
+      uri: require('../images/agents/icons/cypher2.png'),
+      color: '#AAAD98',
+      full_small: require('../images/agents/fullscreen-small/cypher.png'),
+    },
+    Jett: {
+      uri: require('../images/agents/icons/jett2.png'),
+      color: '#76CFDD',
+      full_small: require('../images/agents/fullscreen-small/jett.png'),
+    },
+    Killjoy: {
+      uri: require('../images/agents/icons/killjoy2.png'),
+      color: '#F9DD31',
+      full_small: require('../images/agents/fullscreen-small/killjoy.png'),
+    },
+    Omen: {
+      uri: require('../images/agents/icons/omen2.png'),
+      color: '#4D47F5',
+      full_small: require('../images/agents/fullscreen-small/omen.png'),
+    },
+    Reyna: {
+      uri: require('../images/agents/icons/reyna2.png'),
+      color: '#BA15AD',
+      full_small: require('../images/agents/fullscreen-small/reyna.png'),
+    },
+    Sage: {
+      uri: require('../images/agents/icons/sage2.png'),
+      color: '#61CBB0',
+      full_small: require('../images/agents/fullscreen-small/sage.png'),
+    },
+    Sova: {
+      uri: require('../images/agents/icons/sova2.png'),
+      color: '#3763F6',
+      full_small: require('../images/agents/fullscreen-small/sova.png'),
+    },
+    Viper: {
+      uri: require('../images/agents/icons/viper2.png'),
+      color: '#62C55B',
+      full_small: require('../images/agents/fullscreen-small/viper.png'),
+    },
+    Phoenix: {
+      uri: require('../images/agents/icons/phoenix2.png'),
+      color: '#F38E5E',
+      full_small: require('../images/agents/fullscreen-small/phoenix.png'),
+    },
+  },
+
+  maps: {
+    bind: {
+      uri: require('../images/maps/bind.jpg'),
+    },
+    split: {
+      uri: require('../images/maps/split.jpg'),
+    },
+    haven: {
+      uri: require('../images/maps/haven.jpg'),
+    },
+    ascent: {
+      uri: require('../images/maps/ascent.jpg'),
+    },
+  },
+  tier: {
+    bronze1: {
+      uri: require('../images/tier/bronze1.png'),
+    },
+    bronze2: {
+      uri: require('../images/tier/bronze2.png'),
+    },
+    bronze3: {
+      uri: require('../images/tier/bronze3.png'),
+    },
+    silver1: {
+      uri: require('../images/tier/silver1.png'),
+    },
+    silver2: {
+      uri: require('../images/tier/silver2.png'),
+    },
+    silver3: {
+      uri: require('../images/tier/silver3.png'),
+    },
+    gold1: {
+      uri: require('../images/tier/gold1.png'),
+    },
+    gold2: {
+      uri: require('../images/tier/gold2.png'),
+    },
+    gold3: {
+      uri: require('../images/tier/gold3.png'),
+    },
+    platinum1: {
+      uri: require('../images/tier/platinum1.png'),
+    },
+    platinum2: {
+      uri: require('../images/tier/platinum2.png'),
+    },
+    platinum3: {
+      uri: require('../images/tier/platinum3.png'),
+    },
+    diamond1: {
+      uri: require('../images/tier/diamond1.png'),
+    },
+    diamond2: {
+      uri: require('../images/tier/diamond2.png'),
+    },
+    diamond3: {
+      uri: require('../images/tier/diamond3.png'),
+    },
+    immortal1: {
+      uri: require('../images/tier/immortal1.png'),
+    },
+    immortal2: {
+      uri: require('../images/tier/immortal2.png'),
+    },
+    immortal3: {
+      uri: require('../images/tier/immortal3.png'),
+    },
+    radiant: {
+      uri: require('../images/tier/radiant.png'),
+    },
+  },
+};
+
+export {images};
